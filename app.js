0c8ce636740b27a760ce091fce69d46dedab9000
JeremTer <ternisien09@gmail.com>
2020-11-18 15:40:29 +0100

    add all routes except update story when 5 childs

--- app.js
+++ app.js
@@ -1,41 +1,33 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const db = require('./queries')
+const port = 3000
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+app.use(bodyParser.json())
+app.use(
+    bodyParser.urlencoded({
+      extended: true,
+    })
+)
 
-var app = express();
+app.get('/', (request, response) => {
+  response.json({ info: 'Node.js, Express, and Postgres API' })
+})
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.get('/stories', db.getStories);
+app.get('/stories-end/:ite', db.getEndedStories);
+app.get('/stories/:ite', db.getCurrentStories);
+app.get('/contents/:id', db.getStoryContents);
+app.get('/contents-child/:id', db.getContentChilds);
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.post('/story', db.createStory);
+app.post('/content/:id', db.addContentToNewStory);
+app.post('/content-child/:id', db.addContentChild);
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//app.put('/story', db.updateStory);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`)
+})
---
+++ queries.js
@@ -0,0 +1,153 @@
+const {Client} = require('pg');
+require('dotenv').config()
+const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
+client.connect();
+
+/* Stories */
+const getStories = (request, response) => {
+    client.query('SELECT * FROM story', (error, results) => {
+        if (error) {
+            throw error;
+        }
+        client.end();
+        response.status(200).json(results.rows);
+    });
+}
+
+const getEndedStories = (request, response) => {
+    const ite = parseInt(request.params.ite)
+
+    client.query('SELECT * FROM story WHERE iteration = $1', [ite], (error, results) => {
+        if (error) {
+            throw error;
+        }
+        response.status(200).json(results.rows);
+    })
+}
+
+const getCurrentStories = (request, response) => {
+    const ite = parseInt(request.params.ite)
+
+    client.query('SELECT * FROM story WHERE iteration < $1', [ite], (error, results) => {
+        if (error) {
+            throw error;
+        }
+        response.status(200).json(results.rows);
+    })
+}
+
+const getStoryContents = (request, response) => {
+    const id = parseInt(request.params.id)
+
+    client.query('select content.* from content join story_content sc on content.id = sc.content_id where sc.story_id = $1;', [id], (error, results) => {
+        if (error) {
+            throw error;
+        }
+        response.status(200).json(results.rows);
+    })
+}
+
+const getContentChilds = (request, response) => {
+    const id = parseInt(request.params.id)
+
+    client.query('select content.* from content join content_child cc on content.id = cc.child_id where cc.parent_id = $1;', [id], (error, results) => {
+        if (error) {
+            throw error;
+        }
+        response.status(200).json(results.rows);
+    })
+}
+
+const createStory = (request, response) => {
+    const {name, theme} = request.body;
+
+    client.query('INSERT INTO story (name, theme, iteration) VALUES ($1, $2, $3)', [name, theme, 0], (error, results) => {
+        if (error) {
+            throw error;
+        }
+        response.status(201).send();
+    })
+}
+
+const addContentToNewStory = (request, response) => {
+    const {text} = request.body;
+    const storyId = parseInt(request.params.id);
+
+    client.query('INSERT INTO content (text) VALUES ($1) RETURNING id', [text], (error, results) => {
+        if (error) {
+            throw error;
+        }
+        const insertId = results.rows[0].id;
+        client.query('insert into story_content(story_id, content_id) values ($1, $2)', [storyId, insertId], (error, results) => {
+            if (error) {
+                throw error;
+            }
+            response.status(201).send(`Content added with ID: ${insertId}`);
+        });
+    });
+}
+
+const addContentChild = (request, response) => {
+    const {text} = request.body;
+    const parentId = parseInt(request.params.id);
+
+    client.query('INSERT INTO content (text) VALUES ($1) RETURNING id', [text], (error, results) => {
+        if (error) {
+            throw error;
+        }
+        const insertId = results.rows[0].id;
+        client.query('insert into content_child(parent_id, child_id) values ($1, $2)', [parentId, insertId], (error, results) => {
+            if (error) {
+                throw error;
+            }
+            response.status(201).send(`Content child added with ID: ${insertId}`);
+        });
+    });
+}
+/*
+const updateStory = (request, response) => {
+    const {storyId, contentId} = request.body;
+
+    client.query('delete from story_content where story_id = $1', [storyId], (error, results) => {
+        if (error) {
+            throw error;
+        }
+        client.query('insert into story_content(story_id, content_id) values ($1, $2)', [storyId, contentId], (error, results) => {
+            if (error) {
+                throw error;
+            }
+            client.query('select iteration from story where id = $1', [storyId], (error, results) => {
+                if (error) {
+                    throw error;
+                }
+                console.log(results.rows[0])
+                const iteration = results.rows[0].iteration + 1;
+                client.query('update story set iteration = $1 where id = $2', [iteration, contentId], (error, results) => {
+                    if (error) {
+                        throw error;
+                    }
+                    response.status(201).send(`Story updated with ID: ${storyId}`);
+                });
+            });
+        });
+    });
+}*/
+
+
+module.exports = {
+    getStories,
+    getEndedStories,
+    getCurrentStories,
+    getStoryContents,
+    getContentChilds,
+    createStory,
+    addContentToNewStory,
+    addContentChild,
+    //updateStory
+};
